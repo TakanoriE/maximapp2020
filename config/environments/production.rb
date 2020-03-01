26a7d7242f56248aabc8b0bc637ca470b79a1237
TakanoriE <taka7557@me.com>
2020-03-01 12:29:19 +0900

    production change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,18 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.assets.initialize_on_precompile = false
-
-  config.action_mailer.default_url_options = {host: 'https://maximapp2020.herokuapp.com/' }
-
+  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  host = 'https://maximapp2020.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV["EMAIL_HIDDEN"],
+    :password => ENV["PASSWORD_HIDDEN"],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
