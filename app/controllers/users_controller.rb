1f53416b8de01fc32ba56b7c73bb414e46eed8e3
TakanoriE <taka7557@me.com>
2020-03-01 15:59:56 +0900

    users_controller asc change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :ensure_master_user,{only: [:index]}
 
   def index
-    @users=User.all.order(created_at: :desc)
+    @users=User.all.order(created_at: :asc)
   end
 
   def show
