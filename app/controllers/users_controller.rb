c291d261dbc18285b55802ecc3e2fc521d0c8fa5
TakanoriE <taka7557@me.com>
2020-03-01 15:45:38 +0900

    users_controller change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user=current_user
-    @likes=Like.where(user_id: @user.id).order(created_at: :desc)
+    @likes=Like.where(user_id: @user.id)
   end
 
 end
